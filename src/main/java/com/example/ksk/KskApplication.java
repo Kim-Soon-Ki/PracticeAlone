92eae854d72102cbba22316a7adc7108ba173337
Steve <steve@Rdev.kr>
2024-08-30 14:35:04 +0900

    Initail commit

---
+++ src/main/java/com/example/ksk/KskApplication.java
@@ -0,0 +1,13 @@
+package com.example.ksk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KskApplication.class, args);
+	}
+
+}
